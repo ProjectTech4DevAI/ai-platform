43da48399fa01a844d08b688860164ce38d1d054
Jerome White <jerome@thewex.net>
2025-02-11 14:48:06 +0530

    Use standard Alchemy support types
    See the discussion here: https://github.com/ProjectTech4DevAI/ai-platform/pull/5#issuecomment-2650239134

--- src/app/models/document.py
+++ src/app/models/document.py
@@ -1,67 +1,10 @@
 import uuid
-import functools as ft
-from pathlib import Path
 from datetime import datetime
-from urllib.parse import ParseResult, urlparse, urlunparse
 
 from sqlalchemy import DateTime, ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy.types import TypeDecorator
 
 from ..core.db.database import Base
-
-#
-#
-#
-class MyTypeDecorator(TypeDecorator):
-    impl = String
-
-    @ft.singledispatchmethod
-    def process_bind_param(self, value, dialect):
-        return None
-
-    @process_bind_param.register
-    def _(self, value: str, dialect):
-        return value
-
-    @process_bind_param.register
-    def _(self, value: ParseResult, dialect):
-        return self.to_string(value)
-
-    def process_literal_param(self, value, dialect):
-        param = self.process_bind_param(value, dialect)
-        return 'NULL' if param is None else f"'{param}'"
-
-    def process_result_value(self, value, dialect):
-        return None if value is None else self.from_string(value)
-
-    def to_string(self, value):
-        raise NotImplementedError()
-
-    def from_string(self, value):
-        raise NotImplementedError()
-
-class URLType(MyTypeDecorator):
-    @property
-    def python_type(self):
-        return ParseResult
-
-    def to_string(self, value):
-        return urlunparse(value)
-
-    def from_string(self, value):
-        return urlparse(value)
-
-class PathType(MyTypeDecorator):
-    @property
-    def python_type(self):
-        return Path
-
-    def to_string(self, value):
-        return str(value)
-
-    def from_string(self, value):
-        return self.python_type(value)
 
 #
 #
@@ -95,8 +38,8 @@ class Document(Base):
         primary_key=True,
         unique=True,
     )
-    fname_external: Mapped[Path] = mapped_column(PathType)
-    object_store_url: Mapped[ParseResult] = mapped_column(URLType)
+    fname_external: Mapped[str] = mapped_column(String(512))
+    object_store_url: Mapped[str] = mapped_column(String(2048))
 
     #
     # Record management
