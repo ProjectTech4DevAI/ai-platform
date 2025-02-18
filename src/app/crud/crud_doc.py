4e20e2040758da985c8de7845da4c361c43a235b
Jerome <jerome-white@users.noreply.github.com>
2025-02-18 14:17:22 +0530

    Document Management: Scaffolding (#24)
    * Initial skeleton of the document DB schema (model)
    
    * Use standard Alchemy support types
    
    See the discussion here: https://github.com/ProjectTech4DevAI/ai-platform/pull/5#issuecomment-2650239134
    
    * Stronger DB column specifications
    
    * Maximum URL length consistent with Pydantic specification
    
    https://docs.pydantic.dev/2.2/api/networks/#pydantic.networks.HttpUrl
    
    * Correct way to specify UTC time
    
    https://github.com/python/cpython/pull/103858#issuecomment-1523782382
    
    * Facilitate automatic crud soft delete
    
    * Skeleton of document schema
    
    * The owner is required
    
    * Simplified UUID imports
    
    * FastCRUD document definition
    
    * Group FastCRUD special methods
    
    * Schema info based on data that is required for operation
    
    * Punt on internal updates
    
    * Selection returns the entire structure
    
    * updating with pre-commit
    
    * cleanup formatting
    
    ---------
    
    Co-authored-by: Akhilesh Negi <akhileshnegi.an3@gmail.com>

---
+++ src/app/crud/crud_doc.py
@@ -0,0 +1,19 @@
+from fastcrud import FastCRUD
+
+from ..models.document import Document
+from ..schemas.document import (
+    DocumentCreate,
+    DocumentUpdate,
+    DocumentUpdateInternal,
+    DocumentDelete,
+    DocumentSelect,
+)
+
+crud_docs = FastCRUD[
+    Document,  # ModelType
+    DocumentCreate,  # CreateSchemaType
+    DocumentUpdate,  # UpdateSchemaType
+    DocumentUpdateInternal,  # UpdateSchemaInternalType
+    DocumentDelete,  # DeleteSchemaType
+    DocumentSelect,  # SelectSchemaType
+](Document)
