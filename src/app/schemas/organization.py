ef3674bace6e2b6aebe9b790f25ac5793d4eb02f
nishika26 <nishikayadav26@gmail.com>
2025-02-18 20:16:55 +0530

    changed file names

---
+++ src/app/schemas/organization.py
@@ -0,0 +1,27 @@
+from uuid import UUID
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+from .project import ProjectResponse
+from .credentials import CredentialsResponse
+
+
+class OrganizationBase(BaseModel):
+    name: str
+
+
+class OrganizationCreate(OrganizationBase):
+    pass
+
+
+class OrganizationResponse(OrganizationBase):
+    id: UUID
+    projects: Optional[List["ProjectResponse"]] = []
+    credentials: Optional[List["CredentialsResponse"]] = []
+
+    class Config:
+        orm_mode = True
+
+
+OrganizationResponse.update_forward_refs()
