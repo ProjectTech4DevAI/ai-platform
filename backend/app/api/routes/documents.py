2eee995a075b0994b22ffc6023b4d7770ed7fab7
Jerome White <jerome@thewex.net>
2025-03-11 15:13:24 +0530

    Document route skeleton
    Implements document list based on current user.

---
+++ backend/app/api/routes/documents.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter
+from sqlmodel import select, and_
+
+from app.api.deps import CurrentUser, SessionDep
+from app.models import Document, DocumentList
+
+router = APIRouter(prefix="/documents", tags=["documents"])
+
+
+@router.get(
+    "/ls",
+    response_model=DocumentList,
+)
+def list_docs(
+        session: SessionDep,
+        current_user: CurrentUser,
+        skip: int = 0,
+        limit: int = 100,
+):
+    statement = (select(Document)
+                 .where(
+                     and_(
+                         Document.owner_id == current_user.id,
+                         Document.deleted_at.is_(None),
+                     ),
+                 )
+                 .offset(skip)
+                 .limit(limit))
+    docs = (session
+            .exec(statement)
+            .all())
+
+    return DocumentList(docs=docs)
