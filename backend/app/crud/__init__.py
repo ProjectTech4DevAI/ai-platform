75730a4753c5516c487a20290578c71d66dbb89d
nishika26 <nishikayadav26@gmail.com>
2025-03-12 17:36:23 +0530

    seperated org and project

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -5,3 +5,13 @@ from .user import (
     get_user_by_email,
     update_user,
 )
+from .organization import (
+    create_organization,
+    get_organization_by_id,
+)
+
+from .project import (
+    create_project,
+    get_project_by_id,
+    get_projects_by_organization,
+)
