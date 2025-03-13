15e63896008a7f967661dc68ea457cf78f6cf9ca
sourabhlodha <sourabhlodha@Administrators-MacBook-Pro.local>
2025-03-13 10:28:16 +0530

    remove of item route

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -10,7 +10,6 @@ api_router = APIRouter()
 api_router.include_router(login.router)
 api_router.include_router(users.router)
 api_router.include_router(utils.router)
-api_router.include_router(items.router)
 api_router.include_router(ai_assistant_controller.router)
 if settings.ENVIRONMENT == "local":
     api_router.include_router(private.router)
