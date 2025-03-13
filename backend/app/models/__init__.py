72eed201306a368e806074baecfb4c773bd2209e
avirajsingh7 <asgour2021@gmail.com>
2025-03-13 17:57:58 +0530

    Fix database migrations

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,31 @@
 from .auth import Token, TokenPayload
 from .item import Item, ItemCreate, ItemPublic, ItemsPublic, ItemUpdate
 from .message import Message
+
+from .thread import MessageRequest, AckPayload, CallbackPayload
+
+from .project_user import (
+    ProjectUser,
+    ProjectUserPublic,
+    ProjectUsersPublic,
+)
+
+from .project import (
+    Project,
+    ProjectCreate,
+    ProjectPublic,
+    ProjectsPublic,
+    ProjectUpdate,
+)
+
+from .organization import (
+    Organization,
+    OrganizationCreate,
+    OrganizationPublic,
+    OrganizationsPublic,
+    OrganizationUpdate,
+)
+
 from .user import (
     User,
     UserCreate,
@@ -14,26 +39,3 @@ from .user import (
    UserProjectOrg
 
 )
-from .thread import MessageRequest, AckPayload, CallbackPayload
-   
-
-from .organization import (
-    Organization,
-    OrganizationCreate,
-    OrganizationPublic,
-    OrganizationsPublic,
-    OrganizationUpdate,
-)
-from .project import (
-    Project,
-    ProjectCreate,
-    ProjectPublic,
-    ProjectsPublic,
-    ProjectUpdate,
-)
-from .project_user import (
-    ProjectUser,
-    ProjectUserPublic,
-    ProjectUsersPublic,
-)
-
