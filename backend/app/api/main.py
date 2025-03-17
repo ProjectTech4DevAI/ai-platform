899ebdd8e0c9ed107c1803fa21abae63ccb5b70f
nishika26 <nishikayadav26@gmail.com>
2025-03-17 14:52:02 +0530

    pushing all

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, private, users, utils
+
+from app.api.routes import items, login, private, users, utils,Organization, Project
+
 from app.core.config import settings
 
 api_router = APIRouter()
@@ -8,6 +10,8 @@ api_router.include_router(login.router)
 api_router.include_router(users.router)
 api_router.include_router(utils.router)
 api_router.include_router(items.router)
+api_router.include_router(Organization.router)
+api_router.include_router(Project.router)
 
 
 if settings.ENVIRONMENT == "local":
