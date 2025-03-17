4cbb94f2fcfd15f4ad8d0713884417e9270cb7d5
Jerome White <jerome@thewex.net>
2025-03-17 13:09:15 +0530

    Implementation of file upload

--- backend/app/api/routes/documents.py
+++ backend/app/api/routes/documents.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter
+from pathlib import Path
+from urllib.parse import urlunparse
+
+from fastapi import APIRouter, File, UploadFile
 from sqlmodel import select, and_
 
 from app.api.deps import CurrentUser, SessionDep
+from app.core.cloud import AmazonCloudStorage
 from app.models import Document, DocumentList
 
 router = APIRouter(prefix="/documents", tags=["documents"])
@@ -28,3 +32,28 @@ def list_docs(
             .all())
 
     return DocumentList(docs=docs)
+
+@router.post("/cp")
+def upload_doc(
+        doc: UploadFile,
+        session: SessionDep,
+        current_user: CurrentUser,
+):
+    storage = AmazonCloudStorage(current_user)
+    try:
+        object_store_url = storage.put(doc)
+    except ConnectionError as err:
+        raise HTTPException(status_code=500, detail=str(err))
+    fname_internal = Path(object_store_url.path)
+
+    document = Document(
+        owner_id=user.id,
+        fname_external=Path(doc.filename),
+        fname_internal=fname_internal.stem,
+        object_store_url=urlunparse(object_store_url),
+    )
+    session.add(document)
+    session.commit()
+    session.refresh(document)
+
+    return document.id
