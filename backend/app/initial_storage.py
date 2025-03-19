303c981ce9019ce3ea62a854262c993bfe080841
Jerome White <jerome@thewex.net>
2025-03-19 11:56:45 +0530

    Perform bucket creation at startup

---
+++ backend/app/initial_storage.py
@@ -0,0 +1,27 @@
+import logging
+
+from app.core.cloud import AmazonCloudStorageClient
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def init() -> None:
+    aws = AmazonCloudStorageClient()
+    try:
+        # does the bucket exist...
+        aws.client.head_bucket(Bucket=settings.AWS_S3_BUCKET)
+    except ClientError as err:
+        response = int(err.response['Error']['Code'])
+        if response != 404:
+            raise
+        # ... if not create it
+        aws.client.create_bucket(Bucket=settings.AWS_S3_BUCKET)
+
+def main() -> None:
+    logger.info("START: setup cloud storage")
+    init()
+    logger.info("END: setup cloud storage")
+
+
+if __name__ == "__main__":
+    main()
