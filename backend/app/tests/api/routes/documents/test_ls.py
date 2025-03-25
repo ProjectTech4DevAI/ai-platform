ddc6cf5384a1099a15e36167cb8fe0eaddd0d95b
Jerome White <jerome@thewex.net>
2025-03-26 00:35:17 +0530

    Simplify test parameters

--- backend/app/tests/api/routes/documents/test_ls.py
+++ backend/app/tests/api/routes/documents/test_ls.py
@@ -4,6 +4,7 @@ import functools as ft
 from uuid import UUID
 from pathlib import Path
 from datetime import datetime
+from dataclasses import dataclass
 from urllib.parse import ParseResult, urlunparse
 
 import pytest
@@ -14,7 +15,7 @@ from app.models import Document
 from app.core.config import settings
 from app.tests.utils.document import insert_document, rm_documents
 
-class RouteCreator:
+class Route:
     _empty = ParseResult(*it.repeat('', len(ParseResult._fields)))
 
     def __init__(self, endpoint):
@@ -28,14 +29,35 @@ class RouteCreator:
         path = self.root.joinpath(self.endpoint)
         return self._empty._replace(path=str(path))
 
+@dataclass
+class WebCrawler:
+    client: TestClient
+    superuser_token_headers: dict[str, str]
+
+    @ft.singledispatchmethod
+    def get(self, route: str):
+        return self.client.get(route, headers=self.superuser_token_headers)
+
+    @get.register
+    def _(self, route: Route):
+        return self.get(str(route))
+
+    @get.register
+    def _(self, route: ParseResult):
+        return self.get(urlunparse(route))
+
 @pytest.fixture
 def url():
-    creator = RouteCreator('ls')
-    return creator.to_url()
+    route = Route('ls')
+    return route.to_url()
 
 @pytest.fixture
 def document(db: Session):
     return insert_document(db)
+
+@pytest.fixture
+def crawler(client: TestClient, superuser_token_headers: dict[str, str]):
+    return WebCrawler(client, superuser_token_headers)
 
 @ft.singledispatch
 def to_string(value):
@@ -50,40 +72,33 @@ def _(value: datetime):
     return value.isoformat()
 
 class TestDocumentRouteList:
+    def test_response_is_success(self, url: ParseResult, crawler: Route):
+        response = crawler.get(url)
+        assert response.is_success
+
     def test_empty_db_returns_empty_list(
             self,
             db: Session,
             url: ParseResult,
-            client: TestClient,
-            superuser_token_headers: dict[str, str],
+            crawler: Route,
     ):
         rm_documents(db)
-        docs = (client
-                .get(urlunparse(url), headers=superuser_token_headers)
+        docs = (crawler
+                .get(url)
                 .json()
                 .get('docs'))
 
         assert not docs
 
-    def test_response_is_success(
-            self,
-            url: ParseResult,
-            client: TestClient,
-            superuser_token_headers: dict[str, str],
-    ):
-        response = client.get(urlunparse(url), headers=superuser_token_headers)
-        assert response.is_success
-
     def test_item_reflects_database(
             self,
             url: ParseResult,
             document: Document,
-            client: TestClient,
-            superuser_token_headers: dict[str, str],
+            crawler: Route,
     ):
         source = { x: to_string(y) for (x, y) in dict(document).items() }
-        target = (client
-                  .get(urlunparse(url), headers=superuser_token_headers)
+        target = (crawler
+                  .get(url)
                   .json()
                   .get('docs')
                   .pop())
@@ -93,19 +108,17 @@ class TestDocumentRouteList:
     def test_negative_skip_produces_error(
             self,
             url: ParseResult,
-            client: TestClient,
-            superuser_token_headers: dict[str, str],
+            crawler: Route,
     ):
-        route = urlunparse(url._replace(query='skip=-1'))
-        response = client.get(route, headers=superuser_token_headers)
+        target = url._replace(query='skip=-1')
+        response = crawler.get(urlunparse(target))
         assert response.is_error
 
     def test_negative_limit_produces_error(
             self,
             url: ParseResult,
-            client: TestClient,
-            superuser_token_headers: dict[str, str],
+            crawler: Route,
     ):
-        route = urlunparse(url._replace(query='limit=-1'))
-        response = client.get(route, headers=superuser_token_headers)
+        target = url._replace(query='limit=-1')
+        response = crawler.get(urlunparse(target))
         assert response.is_error
