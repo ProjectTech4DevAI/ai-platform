0a57ce6b67ef975c445f0d7836d815beed5fc011
Jerome White <jerome@thewex.net>
2025-03-25 17:21:51 +0530

    Better usage of fixtures

--- backend/app/tests/crud/documents/test_update.py
+++ backend/app/tests/crud/documents/test_update.py
@@ -9,35 +9,40 @@ from _utils import (
     rm_documents,
 )
 
-class TestState:
-    def __init__(self, db: Session):
-        self.crud = DocumentCrud(db)
-        self.documents = DocumentMaker(db)
-
-    def add(self):
-        return self.crud.update(next(self.documents))
-
-    def get(self):
-        return self.crud.read_many(self.documents.owner_id)
+@pytest.fixture
+def documents(db: Session):
+    rm_documents(db)
+    return DocumentMaker(db)
 
 @pytest.fixture
-def state(db: Session):
-    rm_documents(db)
-    return TestState(db)
+def crud(db: Session):
+    return DocumentCrud(db)
 
 @pytest.mark.usefixtures('clean_db_fixture')
 class TestDatabaseUpdate:
-    def test_update_adds_one(self, state: TestState):
-        before = state.get()
-        state.add()
-        after = state.get()
+    def test_update_adds_one(
+            self,
+            crud: DocumentCrud,
+            documents: DocumentMaker,
+    ):
+        before = crud.read_many(documents.owner_id)
+        crud.update(next(documents))
+        after = crud.read_many(documents.owner_id)
 
         assert len(before) + 1 == len(after)
 
-    def test_sequential_update_is_ordered(self, state: TestState):
-        (a, b) = (state.add() for _ in range(2))
+    def test_sequential_update_is_ordered(
+            self,
+            crud: DocumentCrud,
+            documents: DocumentMaker,
+    ):
+        (a, b) = (crud.update(y) for (_, y) in zip(range(2), documents))
         assert a.created_at <= b.created_at
 
-    def test_insert_does_not_delete(self, state: TestState):
-        document = state.add()
+    def test_insert_does_not_delete(
+            self,
+            crud: DocumentCrud,
+            documents: DocumentMaker,
+    ):
+        document = crud.update(next(documents))
         assert document.deleted_at is None
