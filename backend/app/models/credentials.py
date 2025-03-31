fbca8a181a1b9ef97d1abfe7281ab0f87d9b45e9
nishika26 <nishikayadav26@gmail.com>
2025-03-31 20:38:14 +0530

    models file

--- backend/app/models/credentials.py
+++ backend/app/models/credentials.py
@@ -1,29 +1,31 @@
-from typing import Optional
-
+from typing import Dict, Any, Optional
+import sqlalchemy as sa
 from sqlmodel import Field, Relationship, SQLModel
 
 class CredsBase(SQLModel):
+    organization_id: int = Field(foreign_key="organization.id")
     is_active: bool = True
     valid: bool = True
 
 
 class CredsCreate(CredsBase):
-    openai_api_key: str 
+    credential: Dict[str, Any] = Field(default=None, sa_column=sa.Column(sa.JSON))  # Change JSON to JSONB
 
 
 class CredsUpdate(SQLModel):
-    openai_api_key: str | None = Field(default=None)  
+    credential: Dict[str, Any] | None = Field(default=None, sa_column=sa.Column(sa.JSON))  # Change JSON to JSONB
     is_active: bool | None = Field(default=None)
     valid: bool | None = Field(default=None)
 
 
 class Creds(CredsBase, table=True):
     id: int = Field(default=None, primary_key=True)
-    organization_id: int = Field(foreign_key="organization.id")  
-    openai_api_key: str 
+    credential: Dict[str, Any] = Field(default=None, sa_column=sa.Column(sa.JSON))  # Change JSON to JSONB
+
+    # Relationship to Organization model
+    organization: Optional["Organization"] = Relationship(back_populates="creds")
 
 
 class CredsPublic(CredsBase):
     id: int
-    organization_id: int
-    openai_api_key: str  
+    credential: Dict[str, Any]
