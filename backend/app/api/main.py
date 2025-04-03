e8c9c144e1279aa1fc286e7515edc81d8ee9c560
Priyanshu singh <111607560+PriyanSingh@users.noreply.github.com>
2025-04-03 17:21:03 +0530

    dropped items table

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,13 +1,12 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, private, users, utils, project, organization, project_user, api_keys, threads
+from app.api.routes import login, private, users, utils, project, organization, project_user, api_keys, threads
 from app.core.config import settings
 
 api_router = APIRouter()
 api_router.include_router(login.router)
 api_router.include_router(users.router)
 api_router.include_router(utils.router)
-api_router.include_router(items.router)
 api_router.include_router(threads.router)
 api_router.include_router(organization.router)
 api_router.include_router(project.router)
--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,6 @@
 from sqlmodel import SQLModel
 
 from .auth import Token, TokenPayload
-from .item import Item, ItemCreate, ItemPublic, ItemsPublic, ItemUpdate
 from .message import Message
 
 from .project_user import (
