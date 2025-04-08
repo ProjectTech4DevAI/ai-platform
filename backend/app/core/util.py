3cc4d4fc05b731f2fad303e50be8653770bdf8c8
Jerome White <jerome@thewex.net>
2025-04-08 15:44:47 +0530

    Lift exception handler to core utilities

--- backend/app/core/util.py
+++ backend/app/core/util.py
@@ -1,5 +1,18 @@
+import warnings
 from datetime import datetime, timezone
+
+from fastapi import HTTPException
 
 
 def now():
     return datetime.now(timezone.utc).replace(tzinfo=None)
+
+
+def raise_from_unknown(error: Exception, status_code=500):
+    warnings.warn(
+        'Unexpected exception "{}": {}'.format(
+            type(error).__name__,
+            error,
+        )
+    )
+    raise HTTPException(status_code=status_code, detail=str(error))
