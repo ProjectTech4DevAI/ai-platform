864d2c71096f2bc37e58e728fe560c1009d65c04
avirajsingh7 <asgour2021@gmail.com>
2025-04-10 20:20:07 +0530

    remove unused import

--- backend/app/core/rbac/casbin.py
+++ backend/app/core/rbac/casbin.py
@@ -3,8 +3,7 @@ from casbin import Enforcer
 from casbin_sqlalchemy_adapter import Adapter
 from app.core.db import engine
 from fastapi.concurrency import run_in_threadpool
-from fastapi import HTTPException, status
-from app.crud.project import get_organization_by_project
+
 
 config_path = os.path.join(os.path.dirname(__file__), "rbac_model.conf")
 
