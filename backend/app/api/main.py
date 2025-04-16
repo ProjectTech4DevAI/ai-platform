114adfb019792752bb2e0ab82459292dbacbcfe1
sourabhlodha <sourabhlodha@Administrators-MacBook-Pro.local>
2025-04-16 22:05:33 +0530

    route clean

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -15,7 +15,6 @@ from app.api.routes import (
 from app.core.config import settings
 
 api_router = APIRouter()
-api_router.include_router(login.router)
 api_router.include_router(users.router)
 api_router.include_router(utils.router)
 api_router.include_router(documents.router)
