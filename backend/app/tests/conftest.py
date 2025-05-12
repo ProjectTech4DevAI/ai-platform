512c2eada5a06cf58031576a0b3d68d3da3c3dcf
nishika26 <nishikayadav26@gmail.com>
2025-05-12 17:17:40 +0530

    clean db after test

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -7,7 +7,15 @@ from sqlmodel import Session, delete
 from app.core.config import settings
 from app.core.db import engine, init_db
 from app.main import app
-from app.models import APIKey, Organization, Project, ProjectUser, User, OpenAI_Thread
+from app.models import (
+    APIKey,
+    Organization,
+    Project,
+    ProjectUser,
+    User,
+    OpenAI_Thread,
+    Credential,
+)
 from app.tests.utils.user import authentication_token_from_email
 from app.tests.utils.utils import get_superuser_token_headers
 
@@ -20,6 +28,7 @@ def db() -> Generator[Session, None, None]:
         # Delete data in reverse dependency order
         session.execute(delete(ProjectUser))  # Many-to-many relationship
         session.execute(delete(Project))
+        session.execute(delete(Credential))
         session.execute(delete(Organization))
         session.execute(delete(APIKey))
         session.execute(delete(User))
