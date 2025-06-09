7f93f62f8580294430f9729f713942c4a9e09dda
Aviraj <100823015+avirajsingh7@users.noreply.github.com>
2025-06-09 13:12:39 +0530

    changes in api and crud

--- backend/app/crud/project_user.py
+++ backend/app/crud/project_user.py
@@ -6,7 +6,7 @@ from datetime import datetime, timezone
 from app.core.util import now
 
 
-def is_project_admin(session: Session, user_id: str, project_id: int) -> bool:
+def is_project_admin(session: Session, user_id: int, project_id: int) -> bool:
     """
     Checks if a user is an admin of the given project.
     """
@@ -23,7 +23,7 @@ def is_project_admin(session: Session, user_id: str, project_id: int) -> bool:
 
 # Add a user to a project
 def add_user_to_project(
-    session: Session, project_id: uuid.UUID, user_id: uuid.UUID, is_admin: bool = False
+    session: Session, project_id: int, user_id: int, is_admin: bool = False
 ) -> ProjectUserPublic:
     """
     Adds a user to a project.
@@ -48,7 +48,7 @@ def add_user_to_project(
 
 
 def remove_user_from_project(
-    session: Session, project_id: uuid.UUID, user_id: uuid.UUID
+    session: Session, project_id: int, user_id: int
 ) -> None:
     """
     Removes a user from a project.
@@ -70,7 +70,7 @@ def remove_user_from_project(
 
 
 def get_users_by_project(
-    session: Session, project_id: uuid.UUID, skip: int = 0, limit: int = 100
+    session: Session, project_id: int, skip: int = 0, limit: int = 100
 ) -> tuple[list[ProjectUserPublic], int]:
     """
     Returns paginated users in a given project along with the total count.
@@ -95,7 +95,7 @@ def get_users_by_project(
 
 # Check if a user belongs to an at least one project in organization
 def is_user_part_of_organization(
-    session: Session, user_id: uuid.UUID, org_id: int
+    session: Session, user_id: int, org_id: int
 ) -> bool:
     """
     Checks if a user is part of at least one project within the organization.
