e82d1cb2f87538c02560320ce7fdaa112b613647
nishika26 <nishikayadav26@gmail.com>
2025-06-13 14:43:45 +0530

    main alignment

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -33,6 +33,5 @@ api_router.include_router(threads.router)
 api_router.include_router(users.router)
 api_router.include_router(utils.router)
 
-
 if settings.ENVIRONMENT == "local":
     api_router.include_router(private.router)
