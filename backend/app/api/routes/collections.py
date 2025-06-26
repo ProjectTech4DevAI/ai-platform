1a56355282e1c05c81e773cb21e27f269fa0931a
Aviraj <100823015+avirajsingh7@users.noreply.github.com>
2025-06-26 10:49:46 +0530

    pre commit run

--- backend/app/api/routes/collections.py
+++ backend/app/api/routes/collections.py
@@ -134,15 +134,11 @@ class CallbackHandler:
 
 class SilentCallback(CallbackHandler):
     def fail(self, body):
-        logger.info(
-            f"[SilentCallback.fail] Silent callback failure"
-        )
+        logger.info(f"[SilentCallback.fail] Silent callback failure")
         return
 
     def success(self, body):
-        logger.info(
-            f"[SilentCallback.success] Silent callback success"
-        )
+        logger.info(f"[SilentCallback.success] Silent callback success")
         return
 
 
@@ -309,8 +305,6 @@ def do_delete_collection(
     payload: ResponsePayload,
 ):
     if request.callback_url is None:
-        print("Using silent callback")
-        print(f"Payload: {payload}")
         callback = SilentCallback(payload)
     else:
         callback = WebHookCallback(request.callback_url, payload)
