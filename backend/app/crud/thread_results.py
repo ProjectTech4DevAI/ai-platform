d8c5d010ad10b9c0cab61a4bdbebdad3f28de741
Aviraj <100823015+avirajsingh7@users.noreply.github.com>
2025-08-05 11:12:49 +0530

    refactor

--- backend/app/crud/thread_results.py
+++ backend/app/crud/thread_results.py
@@ -18,13 +18,13 @@ def upsert_thread_result(session: Session, data: OpenAIThreadCreate):
         existing.error = data.error
         existing.updated_at = datetime.utcnow()
         logger.info(
-            f"[upsert_thread_result] Updated existing thread result with ID: {mask_string(data.thread_id)}"
+            f"[upsert_thread_result] Updated existing thread result in the db with ID: {mask_string(data.thread_id)}"
         )
     else:
         new_thread = OpenAI_Thread(**data.dict())
         session.add(new_thread)
         logger.info(
-            f"[upsert_thread_result] Created new thread result with ID: {mask_string(new_thread.thread_id)}"
+            f"[upsert_thread_result] Created new thread result in the db with ID: {mask_string(new_thread.thread_id)}"
         )
     session.commit()
 
