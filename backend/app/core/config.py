4d0c11af9900865db5d1dc4ffa419a2b28012e81
Akhilesh Negi <akhileshnegi@Akhileshs-MacBook-Pro.local>
2025-08-08 10:52:11 +0530

    skipping warning in development and testing

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -106,7 +106,7 @@ class Settings(BaseSettings):
                 f'The value of {var_name} is "changethis", '
                 "for security, please change it, at least for deployments."
             )
-            if self.ENV == "development":
+            if self.ENV == ["development", "testing"]:
                 warnings.warn(message, stacklevel=1)
             else:
                 raise ValueError(message)
