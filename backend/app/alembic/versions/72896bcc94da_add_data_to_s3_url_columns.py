6fb4e5fe9a0789e8bafc513b34660694492315e3
nishika26 <nishikayadav26@gmail.com>
2025-08-24 22:51:06 +0530

    alembic file for adding and removing columns

---
+++ backend/app/alembic/versions/72896bcc94da_add_data_to_s3_url_columns.py
@@ -0,0 +1,64 @@
+"""add data to s3 url columns
+
+Revision ID: 72896bcc94da
+Revises: e317d05f49e4
+Create Date: 2025-08-22 00:44:45.426211
+
+"""
+from alembic import op
+import sqlalchemy as sa
+import sqlmodel.sql.sqltypes
+
+
+# revision identifiers, used by Alembic.
+revision = "72896bcc94da"
+down_revision = "e317d05f49e4"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_column("fine_tuning", "testing_file_id")
+    op.add_column(
+        "fine_tuning",
+        sa.Column("train_data_url", sqlmodel.sql.sqltypes.AutoString(), nullable=True),
+    )
+    op.add_column(
+        "fine_tuning",
+        sa.Column("test_data_url", sqlmodel.sql.sqltypes.AutoString(), nullable=True),
+    )
+    op.add_column(
+        "model_evaluation",
+        sa.Column(
+            "test_data_url",
+            sqlmodel.sql.sqltypes.AutoString(),
+            nullable=False,
+            server_default="",
+        ),
+    )
+    op.add_column(
+        "model_evaluation",
+        sa.Column(
+            "prediction_data_url", sqlmodel.sql.sqltypes.AutoString(), nullable=True
+        ),
+    )
+    op.alter_column(
+        "model_evaluation", "testing_file_id", existing_type=sa.VARCHAR(), nullable=True
+    )
+
+
+def downgrade():
+    op.drop_column("fine_tuning", "test_data_url")
+    op.drop_column("fine_tuning", "train_data_url")
+    op.add_column(
+        "fine_tuning",
+        sa.Column("testing_file_id", sa.VARCHAR(), autoincrement=False, nullable=True),
+    )
+    op.drop_column("model_evaluation", "prediction_data_url")
+    op.drop_column("model_evaluation", "test_data_url")
+    op.alter_column(
+        "model_evaluation",
+        "testing_file_id",
+        existing_type=sa.VARCHAR(),
+        nullable=False,
+    )
