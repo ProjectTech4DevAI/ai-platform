77677498887bf15bdbb68b8ecffd2d4bfaa3cd33
Aviraj <100823015+avirajsingh7@users.noreply.github.com>
2025-09-17 16:44:54 +0530

    Refactor response api: 1) Move models from route to models folder 2) fix process response to handle callback response

---
+++ backend/app/models/response.py
@@ -0,0 +1,47 @@
+from sqlmodel import SQLModel
+
+
+class ResponsesAPIRequest(SQLModel):
+    assistant_id: str
+    question: str
+    callback_url: str | None= None
+    response_id: str | None= None
+
+    class Config:
+        extra = "allow"
+
+
+class ResponsesSyncAPIRequest(SQLModel):
+    model: str
+    instructions: str
+    vector_store_ids: list[str]
+    max_num_results: int = 20
+    temperature: float = 0.1
+    response_id: str | None = None
+    question: str
+
+    class Config:
+        extra = "allow"
+
+
+class Diagnostics(SQLModel):
+    input_tokens: int
+    output_tokens: int
+    total_tokens: int
+    model: str
+
+
+class FileResultChunk(SQLModel):
+    score: float
+    text: str
+
+
+class CallbackResponse(SQLModel):
+    status: str
+    response_id: str
+    message: str
+    chunks: list[FileResultChunk]
+    diagnostics: Diagnostics | None = None
+
+    class Config:
+        extra = "allow"
