bac48ce9590445181630d62494781ede315224cb
rkritika1508 <rkritika1508@gmail.com>
2025-12-02 03:49:39 +0530

    Cleaned up code

--- backend/app/safety/guardrail_config.py
+++ backend/app/safety/guardrail_config.py
@@ -5,21 +5,6 @@ from app.safety.validators.lexical_slur import LexicalSlurSafetyValidatorConfig
 
 # todo this could be improved by having some auto-discovery mechanism inside
 # validators. We'll not have to list every new validator like this. 
-# from .validators.lexical_slur import LexicalSlurSafetyValidatorConfig
-
-
-# class PIISafetyValidatorConfig(BaseValidatorConfig):
-#     type: Literal["pii_remover"]
-#     language: str = "en"
-#     entity_types: List[str] = ["ALL"]
-#     threshold: float = 0.5
-#     # language_detector: Optional[LanguageDetector] = None
-#     # validator_cls: ClassVar = <add-name>
-
-# class GenderAssumptionBiasSafetyValidatorConfig(BaseValidatorConfig):
-#     type: Literal["gender_assumption_bias"]
-#     languages: List[str] = ["en", "hi"]
-#     # validator_cls: ClassVar = <add-name>
 
 ValidatorConfigItem = Annotated[
     # Union[PIISafetyValidatorConfig, LexicalSlurSafetyValidatorConfig, GenderAssumptionBiasSafetyValidatorConfig],
