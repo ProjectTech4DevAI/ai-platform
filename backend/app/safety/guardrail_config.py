a3b013b212150c10e7e4ea2904143a331d8290b2
rkritika1508 <rkritika1508@gmail.com>
2025-12-03 13:13:33 +0530

    Fixed guardrail config

--- backend/app/safety/guardrail_config.py
+++ backend/app/safety/guardrail_config.py
@@ -4,7 +4,7 @@ from typing import List, Union, Annotated
 # todo this could be improved by having some auto-discovery mechanism inside
 # validators. We'll not have to list every new validator like this.
 from app.safety.validators.lexical_slur import LexicalSlurSafetyValidatorConfig 
-from app.safety.validators.ban_list_validator_config import BanListSafetyValidatorConfig
+from app.safety.validators.ban_list_safety_validator_config import BanListSafetyValidatorConfig
 
 ValidatorConfigItem = Annotated[
     # future validators
